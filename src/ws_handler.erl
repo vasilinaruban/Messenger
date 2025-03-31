89776733fd2a1b0aedf381900134abf85571ae09
vasilina <vasilina.rub@mail.ru>
2025-03-31 08:53:32 +0700

    Fix displaying messages

--- src/ws_handler.erl
+++ src/ws_handler.erl
@@ -8,37 +8,29 @@
 -export([terminate/3]).
 
 init(Req0, State) ->
-    % Parse query string from the request
     Qs = cowboy_req:parse_qs(Req0),
     TokenParam = proplists:get_value(<<"token">>, Qs),
 
     case TokenParam of
         undefined ->
-            % If no token is provided, reject the connection
             Req1 = cowboy_req:reply(401, #{}, <<"Missing or invalid token">>, Req0),
             {shutdown, Req1, State};
         TokenValue ->
-            % Verify the token
             case verify_token(binary_to_list(TokenValue)) of
                 {ok, Username} ->
-                    % If the token is valid, proceed with the WebSocket connection
-                    io:format("WebSocket connected for user: ~p Pid: ~p~n", [Username, self()]),
-                    user_registry:add_user(Username, self()),
-                    io:format("All users: ~p~n", [user_registry:get_all_users()]),
-                    {cowboy_websocket, Req0, #{username => Username, pid => self()}};
+                    io:format("WebSocket connected for user: ~p~n", [Username]),
+                    Pid = maps:get(pid, Req0, undefined),
+                    user_registry:add_user(Username, Pid),
+                    {cowboy_websocket, Req0, #{username => Username}};
                 {error, Reason} ->
-                    % If the token is invalid, reject the connection
                     Msg = io_lib:format("Invalid token: ~p", [Reason]),
                     Req1 = cowboy_req:reply(401, #{}, list_to_binary(Msg), Req0),
                     {shutdown, Req1, State}
             end
     end.
 
-websocket_init(Req, _Opts, State = #{username := _Username}) ->
-    MonitorRef = erlang:monitor(process, self()),
-    {ok, TRef} = timer:apply_after(60000, self(), idle_timeout),
-    {ok, Req, State#{monitor_ref => MonitorRef, idle_timer => TRef}}.
-
+websocket_init(Req, _Opts, State) ->
+    {cowboy_websocket, Req, State}.
 
 websocket_handle({text, Msg}, State = #{username := Sender}) ->
     try jsx:decode(Msg, [return_maps]) of
@@ -74,40 +66,26 @@ websocket_handle({text, Msg}, State = #{username := Sender}) ->
 websocket_handle(_Data, State) ->
     {ok, State}.
 
-
 websocket_info({send_message, Message}, State) ->
     {reply, {text, Message}, State};
-
-websocket_info({'DOWN', _Ref, process, Pid, Reason}, State) ->
-    io:format("Process ~p died: ~p~n", [Pid, Reason]),
-    user_registry:handle_down(Pid, Reason),
-    {shutdown, State};
-
-websocket_info(idle_timeout, #{username := Username} = State) ->
-    io:format("Closing idle connection for ~p~n", [Username]),
-    {shutdown, State};
 
 websocket_info(_Info, State) ->
     {ok, State}.
 
+terminate(_Reason, _Req, State) ->
+    case is_map(State) of
+        true -> 
+            Username = maps:get(username, State),
+            io:format("WebSocket connection closed for user ~p~n", [Username]),
+            user_registry:remove_user(Username),
+            ok;
+        _ -> ok
+    end.
 
-terminate(_Reason, _Req, #{monitor_ref := Ref, idle_timer := TRef, username := Username}) ->
-    timer:cancel(TRef),
-    erlang:demonitor(Ref, [flush]),
-    user_registry:remove_user(Username),
-    ok;
-
-terminate(_Reason, _Req, _State) ->
-    ok.
-
-% Function to verify the custom token
 verify_token(Token) ->
     try
-        % Decode the base64 token
         Decoded = base64:decode(Token),
-        % Split the decoded value into username and password
         [Username, Password] = binary:split(Decoded, <<":">>),
-        % Authenticate the user
         case user_storage:authenticate(binary_to_list(Username), binary_to_list(Password)) of
             true ->
                 {ok, Username};
@@ -118,5 +96,3 @@ verify_token(Token) ->
         _:_ ->
             {error, invalid_token}
     end.
-
-
